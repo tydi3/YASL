388502ffb463cf873c3a2683cc9c61d19e83a0a0
AO™ <anthony@osawere.com>
2024-11-30 18:59:49 +0100

    Rebranded

--- js/yale.js
+++ js/yale.js
@@ -1,4 +1,4 @@
-/* tydi.js » Yale™ Library - Yet Another Laravel Elevator © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License */
+/* yale.js » Yale™ Library - Yet Another Laravel Elevator © 2024 ∞ AO™ • @osawereao • www.osawere.com ∞ Apache License */
 
 function oFormLogout(formID = "logoutForm") {
 	let formElem = document.getElementById(formID);
